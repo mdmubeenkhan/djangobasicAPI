0f4703922295836b72da9716e3b6cd890173c290
mubeen <mubbeen@gmail.com>
2021-11-15 10:07:14 +0530

    APIView is implemented apart from Viewset

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from testapp.views import EmployeeView
+from testapp.views import EmployeeView, EmployeeAPIView
 
 router = DefaultRouter()
 router.register('api', EmployeeView, basename='apis')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('apiview/', EmployeeAPIView.as_view()),
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -2,8 +2,50 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from testapp.models import Employee
 from testapp.serializers import EmployeeSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from io import BytesIO
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
 class EmployeeView(ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+
+class EmployeeAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        qs = Employee.objects.all()
+        serializer = EmployeeSerializer(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        emp_serializer = EmployeeSerializer(data=python_data)
+        if emp_serializer.is_valid():
+            emp_serializer.save()
+            return Response('Employee record inserted.')        
+        return Response(emp_serializer.errors)
+
+    def put(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = BytesIO(json_data)
+        python_data = JSONParser().parse(stream=stream)
+        emp_no = python_data.get('eno')
+        emp_obj = Employee.objects.get(eno=emp_no)
+        emp_serializer = EmployeeSerializer(emp_obj, data=python_data, partial=True)
+        if emp_serializer.is_valid():
+            emp_serializer.save()
+            return Response('Employee record updated.')        
+        return Response(emp_serializer.errors)
+
+    def delete(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = BytesIO(json_data)
+        python_data = JSONParser().parse(stream=stream)
+        emp_no = python_data.get('eno')
+        emp_obj = Employee.objects.get(eno=emp_no)
+        emp_obj.delete()
+        return Response('Employee record deleted.')  
